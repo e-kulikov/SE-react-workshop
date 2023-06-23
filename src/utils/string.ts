7cd1c815d9c1206857965b6ea63a98cd3560f00d
e.kulikov <e.kulikov@godeltech.com>
2023-06-23 17:16:44 +0200

    Utils: implement a function to convert dash-case in Capital Letter Separate Words

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,2 +1,8 @@
 export const firstCapital = ([first, ...rest]: string): string =>
-  `${first.toUpperCase()}${rest.join('')}`
+  `${first.toUpperCase()}${rest.join('')}`;
+
+export const getPokemonTitle = (name: string): string =>
+  name
+    .split('-')
+    .reduce((result, part) => result + ' ' + firstCapital(part), '')
+    .trim();
