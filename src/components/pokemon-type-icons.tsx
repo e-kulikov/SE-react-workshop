f0b01f61891fb1bc76aff4159022e645a10d1c2c
e.kulikov <e.kulikov@godeltech.com>
2023-06-23 17:25:03 +0200

    Component: add additional pokemon type icons

--- src/components/pokemon-type-icons.tsx
+++ src/components/pokemon-type-icons.tsx
@@ -2,11 +2,16 @@ import { ReactElement } from 'react';
 import {
   BugTwoTone,
   CloudTwoTone,
+  CompassTwoTone,
   ExclamationCircleTwoTone,
   ExperimentTwoTone,
   FireTwoTone,
+  LikeTwoTone,
   PictureTwoTone,
+  SettingTwoTone,
   SlidersTwoTone,
+  StarTwoTone,
+  ThunderboltTwoTone,
 } from '@ant-design/icons';
 
 export const TypeIcon: Record<string, () => ReactElement> = {
@@ -16,5 +21,10 @@ export const TypeIcon: Record<string, () => ReactElement> = {
   grass: () => <PictureTwoTone twoToneColor="lightgreen" />,
   poison: () => <ExperimentTwoTone twoToneColor="violet" />,
   water: () => <SlidersTwoTone twoToneColor="blue" />,
+  electric: () => <ThunderboltTwoTone twoToneColor="red" />,
+  ground: () => <SettingTwoTone twoToneColor="dark" />,
+  normal: () => <CompassTwoTone twoToneColor="lightblue" />,
+  fairy: () => <StarTwoTone twoToneColor="pink" />,
+  fighting: () => <LikeTwoTone twoToneColor="red" />,
   default: () => <ExclamationCircleTwoTone twoToneColor="gray" />,
 };
