c5669234e89f9aa8fb3d09b7bb122f5fb42c2e41
e.kulikov <e.kulikov@godeltech.com>
2023-07-14 14:19:56 +0200

    Implement tests
    - tests are implemented and optimised

--- src/components/pokemon-image.test.ts
+++ src/components/pokemon-image.test.ts
@@ -11,19 +11,68 @@ const useRefSpy = jest.spyOn(React, 'useRef');
 describe('pokemon image', () => {
   describe('PokemonImage component', () => {});
   describe('useCarouselAutoplay hook', () => {
+    const TOGGLE_STATE_MOCK = jest.fn();
+    const TOGGLE_TRUE_MOCK = jest.fn();
+    const TOGGLE_FALSE_MOCK = jest.fn();
+    const GO_TO_MOCK = jest.fn();
+
+    beforeAll(() => {
+      useRefSpy.mockReturnValue({
+        current: {
+          goTo: GO_TO_MOCK,
+        },
+      });
+      useToggleSpy.mockReturnValue([
+        TOGGLE_STATE_MOCK,
+        { toggleTrue: TOGGLE_TRUE_MOCK, toggleFalse: TOGGLE_FALSE_MOCK },
+      ] as any as ReturnType<typeof useToggle>);
+    });
+
+    afterAll(() => {
+      jest.restoreAllMocks();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks(); // calls are cleared, implementation is saved, mock is saved
+      // jest.resetAllMocks(); // calls are cleared, implementation is cleared, mock is saved
+      // jest.restoreAllMocks(); // clear everything
+    });
+
     it('should call useToggle under the hood', () => {
       renderHook(useCarouselAutoplay);
 
       expect(useToggleSpy).toHaveBeenCalled();
     });
-    it.todo('should call useRef under the hood');
-    it.todo('should return state and reference');
-    it.todo(
-      'should call `toggleTrue` of `useToggle` when calling `startAutoplay` method'
-    );
-    it.todo(
-      'should call `toggleFalse` of `useToggle` when calling `stopAutoplay` method'
-    );
-    it.todo('should call `.goTo(0)` exposed method of referenced component');
+    it('should call useRef under the hood', () => {
+      renderHook(useCarouselAutoplay);
+
+      expect(useRefSpy).toHaveBeenCalled();
+    });
+    it('should return state', () => {
+      const { result } = renderHook(useCarouselAutoplay);
+      expect(result.current?.[0]).toBe(TOGGLE_STATE_MOCK);
+    });
+    it('should call `toggleTrue` of `useToggle` when calling `startAutoplay` method', () => {
+      const { result } = renderHook(useCarouselAutoplay);
+
+      result.current?.[2].startAutoplay();
+      expect(TOGGLE_TRUE_MOCK).toHaveBeenCalled();
+    });
+    it('should call `.goTo(0)` exposed method of referenced component', () => {
+      const { result } = renderHook(useCarouselAutoplay);
+
+      result.current?.[2].stopAutoplay();
+      expect(GO_TO_MOCK).toHaveBeenCalledTimes(1);
+      expect(GO_TO_MOCK).toHaveBeenCalledWith(0);
+    });
+
+    describe('description', () => {
+      it('should call `toggleFalse` of `useToggle` when calling `stopAutoplay` method', () => {
+        const { result } = renderHook(useCarouselAutoplay);
+
+        result.current?.[2].stopAutoplay();
+        expect(TOGGLE_FALSE_MOCK).toHaveBeenCalled();
+      });
+    });
   });
 });
